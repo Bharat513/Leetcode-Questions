8733c82357560e13d938bce33c51a15b55d7ef8c
Bharat Singh <110762387+Bharat513@users.noreply.github.com>
2024-01-18 19:32:37 +0530

    Time: 3 ms (89.91%) | Memory: 13.6 MB (38.21%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,57 +1,49 @@
 class Solution {
-
-    
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<int, int>> pq;
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < m; col++){
-                if(grid[row][col] == 2){
-                    pq.push({row, col});
-                    vis[row][col] = 1;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        
+        queue<pair<pair<int,int>,int>> q;
+        int mintime=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    vis[i][j]=2;
+                    q.push({{i,j},0});
                 }
-            }
-        }
-
-        //bfs
-        int cost = 0;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        while(!pq.empty()){
-            bool flag = false;
-            int size = pq.size();
-            for(int i = 0; i < size; i++){
-                int r = pq.front().first;
-                int c = pq.front().second;
-                pq.pop();
-                for(int j = 0; j < 4; j++){
-                            int nr = r + drow[j];
-                            int nc = c + dcol[j];
-                            if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
-                                pq.push({nr, nc});
-                                vis[nr][nc] = 1;
-                                flag = true;
-                            }
-                        
-                    
-                } 
                 
             }
-            if(flag)cost++;
         }
 
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < m; col++){
-                if(!vis[row][col] && grid[row][col] == 1){
-                    return -1;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            mintime=t;
+            for(int i=0;i<4;i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1 ){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=2;
                 }
+
             }
         }
-        return cost;
-
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]!=2 && grid[i][j]==1){
+                    return -1;
+                }
+                
+            }
+        }
+        return mintime;
 
     }
 };
