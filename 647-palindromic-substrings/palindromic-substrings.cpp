b82135305a8f23409a1a6c2932dedac2b7635988
Bharat Singh <110762387+Bharat513@users.noreply.github.com>
2024-02-10 10:03:35 +0530

    Time: 4 ms (76.20%) | Memory: 7.5 MB (74.15%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,21 +1,31 @@
 class Solution {
-    bool palindrom(string s){
-        int n = s.size();
-        string revs1 = string(s.rbegin(), s.rend());
-        for(int i = 0; i < n/2; i++){
-            if(s[i] != revs1[i])return false;
-        }
-        return true;
-    }
+    // bool palindrom(string s){
+    //     int n = s.size();
+    //     string revs1 = string(s.rbegin(), s.rend());
+    //     for(int i = 0; i < n/2; i++){
+    //         if(s[i] != revs1[i])return false;
+    //     }
+    //     return true;
+    // }
 public:
     int countSubstrings(string s) {
         int n = s.size();
         int ans = 0;
         for(int i = 0; i < n; i++){
-            for(int j = 1; i + j <= n; j++){
-                string subStr = s.substr(i, j);
-                if(palindrom(subStr)) ans++;
+            int left = i, right = i;
+            while(left >= 0 && right < n){
+                if(s[left] == s[right]){
+                    left--, right++;
+                    ans++;
+                }else break;
+            }
 
+            left = i, right = i + 1;
+            while(left >= 0 && right < n){
+                if(s[left] == s[right]){
+                    left--, right++;
+                    ans++;
+                }else break;
             }
         }
         return ans;
