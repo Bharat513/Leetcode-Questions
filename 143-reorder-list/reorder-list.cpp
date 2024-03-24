786db9d777d4e4fea54a832ed934d789b3154ec3
Bharat Singh <110762387+Bharat513@users.noreply.github.com>
2024-03-24 10:46:18 +0530

    Time: 22 ms (76.30%) | Memory: 21.2 MB (69.06%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,29 +1,43 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-    ListNode* helper(ListNode* head){
-        if(head == NULL || head->next == NULL)return head;
-        ListNode* temp = head;
-        while(temp->next->next){
-            temp = temp->next;
-        }
-        ListNode* last = temp->next;
-        temp->next = NULL;
-        ListNode* newhead = head->next;
-        head->next = last;
-        if(last)last->next = helper(newhead);
-        return head;
+class Solution{
+    ListNode* helper(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
 
+        // Step 1: Find the middle of the linked list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        ListNode* mid = slow;
+
+        // Step 2: Reverse the second half of the linked list
+        ListNode* prev = NULL;
+        ListNode* curr = mid;
+        ListNode* nextNode;
+        while (curr) {
+            nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        ListNode* reversed = prev;
+
+        // Step 3: Interleave the nodes of the first and reversed second halves
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = reversed;
+        while (secondHalf->next) {
+            ListNode* temp = firstHalf->next;
+            firstHalf->next = secondHalf;
+            firstHalf = temp;
+
+            temp = secondHalf->next;
+            secondHalf->next = firstHalf;
+            secondHalf = temp;
+        }
+        return head;
     }
-public:
+    public:
     void reorderList(ListNode* head) {
         ListNode* tempHead = head;
         helper(tempHead);
