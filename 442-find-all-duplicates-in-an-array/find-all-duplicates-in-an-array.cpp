9257dc2822074155a063162fc294320bfd3a64da
Bharat Singh <110762387+Bharat513@users.noreply.github.com>
2024-03-26 13:40:00 +0530

    Time: 58 ms (36.75%) | Memory: 42 MB (33.33%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,25 +1,18 @@
 class Solution {
-    std::vector<int> numCount;
 public:
-
-    vector<int> findDuplicates(vector<int>& n)
-    {
-        vector<int> duplicates;
-        
-        for(int i=0;i<n.size();i++)
-        { 
-            // evaluate index of selected element if array would have been sorted..
-            int indexOfElem = abs(n[i]) - 1;
-            
-            // negate the element
-            n[indexOfElem] = - n[indexOfElem];
-            
-            // if this element is positive means double negation had happened which 
-            // means we editied this element twice.
-            if(n[indexOfElem]>0)
-                duplicates.push_back(indexOfElem+1);
-            
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> st;
+        for(int i = 0; i < n; i++){
+            while(nums[i] != i + 1){
+                if(nums[i] == nums[nums[i] - 1]){
+                    st.insert(nums[i]);
+                    break;
+                }
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
-        return duplicates;
+        vector<int> ans(st.begin(), st.end());
+        return ans;
     }
 };
